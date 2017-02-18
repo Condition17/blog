a374e6851554b91ecff9338a944b284a496b49ca
Cristian Conache <cconache@gmail.com>
2017-02-18 02:14:23 +0200

    logo position set and added scroll js file and apropriate links

---
+++ assets/js/scroll.js
@@ -0,0 +1,14 @@
+/*var fadeOut = function(){
+	header = document.querySelector(".main-header");
+	header.style.opacity = 1-document.scrolltop/header.height;
+	console.log(header.style.opacity);
+}
+
+var fadeIn = function(){
+	header = document.querySelector(".main-header");
+	header.style.opacity += document.scrolltop/header.height;
+	console.log(header.style.opacity);
+}
+window.onload = function(){
+	window.addEventListener("scroll",fadeOut);
+}*/
